1bce55720246c43076eb15931c0e2638bd657e36
Antoine Carrette <71089756+Eriuden@users.noreply.github.com>
2023-08-12 11:23:05 +0200

    Update page.js

--- src/app/editTopic/[id]/page.js
+++ src/app/editTopic/[id]/page.js
@@ -5,7 +5,7 @@ const getTopicById = async(id)=> {
   try{
     const res= await fetch(`http://localhost:3000/api/topics/${id}`,
     {cache:"no-store",
-  })
+    })
 
   if(!res.ok){
     throw new Error("opération échouée")
